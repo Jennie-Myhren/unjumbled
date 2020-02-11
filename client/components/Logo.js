325cebeb5b29470f03d69e47eebf10d75a039bb2
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-02-11 15:30:43 -0600

    Logo typeface selected

--- client/components/Logo.js
+++ client/components/Logo.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Logo = () => {
-  return <img id="logo" src="./public/bookshelf.jpg" />;
+  // return <img id="logo" src="./public/bookshelf.jpg" />;
+  return <h1 id="logo">Unjumbled</h1>;
 };
 
 export default Logo;

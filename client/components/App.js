a6e6735e275b66f503fd6ef884f98e51ccc58f43
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-02-11 15:17:14 -0600

    Added NavBar (no buttons) with "logo"

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Navbar } from './index';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>Welcome to UNJUMBLED</h1>
     </div>
   );
---
+++ client/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Navbar = () => {
+  //TK: ADD DB & UPDATE
+  const user = 'Jennie';
+  const loggedIn = user === true;
+
+  return (
+    <div id="navbar">
+      <div className="container">
+        <Logo />
+        {user ? (
+          <p className="navText">Welcome, {user}</p>
+        ) : (
+          <p className="navText">Welcome, Guest</p>
+        )}
+        {/* TK: MAKE THIS CLICKABLE! */}
+        {loggedIn ? (
+          <p className="navText">Sign Out</p>
+        ) : (
+          <p className="navText">Sign In</p>
+        )}
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

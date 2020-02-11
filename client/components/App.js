0113ade7f9169116e0d102a8da2131b300f6fa5e
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-02-11 16:06:19 -0600

    added placeholder image to landing page

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,11 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to UNJUMBLED</h1>
+      <div className="container">
+        <img src="https://i.picsum.photos/id/24/1213/450.jpg" />
+        {/* <img src="https://i.picsum.photos/id/223/1228/816.jpg" /> */}
+        <h1 className="centered">Welcome to your online office!</h1>
+      </div>
     </div>
   );
 };
